7885dd5f797df336df27498961e2209dfa2a56fb
Karashily <ahmed.elkarashily@gmail.com>
2020-05-29 17:57:48 +0200

    initial commit

---
+++ Placer_class.py
@@ -0,0 +1,173 @@
+''' This module defines a placer class that is designed based on simulated
+annealing optimization of a half-parameter bounding box cost function.'''
+import random as RND
+from numpy import exp, std, mean,log, absolute, floor
+
+
+class sa_placer_agent():
+    def __init__(self, chip_obj, max_iterations, verbose=0):
+        self.verbose = verbose
+        self.temp_schedule = []
+        self.max_iter = max_iterations
+        self.T = 0
+        self.num_iter = 0
+        self.chip = chip_obj
+        self.current_delta_cost = 0
+        self.sigma=0
+        self.max_param_sample = 20000
+        self.no_change = 0
+        self.accepted = True
+        self.num_rejection = 0
+        self.num_acceptance = 0
+        self.epsilon = 1
+        self.rejection=0
+        self.last_good_move = 0
+
+    def param_initialization(self):
+        '''
+        The initialization is based on improvements suggested by the following
+        Paper:
+        VLSI cell placement Techniques
+        K shahkoor and P. mazumder
+        '''
+
+        # Kai0 is the initial desired acceptance probability:
+        self.Kai0 = .90
+
+        # Initializing variables:
+        DELTA_C=[]
+        DELTA_C_PLUS = []
+
+        m2=0
+        m1=0
+
+        for i in range(self.max_param_sample):
+            cell_i, cell_j, delta_cost = self.random_swap()
+            DELTA_C.append(delta_cost)
+
+            if delta_cost > 0:
+                DELTA_C_PLUS.append(delta_cost)
+                m2 += 1
+            elif delta_cost <=0:
+                m1 += 1
+
+        self.sigma = round(std(DELTA_C),10)
+
+
+        DELTA_C_PLUS_AVG = mean(DELTA_C_PLUS)
+
+        self.T = DELTA_C_PLUS_AVG/ \
+        (log(m2*1.0/(m2*self.Kai0  - (1-self.Kai0 )*m1)))
+
+        #self.T =20*self.sigma
+
+        if self.verbose > 0:
+            print('-'*50)
+            print('SA Initial Parameters:')
+            print("Average Delta C+ of sample:" , DELTA_C_PLUS_AVG)
+            print("Sigma of sample: ", self.sigma)
+            print('# Cost decreasing samples:', m1)
+            print('# Cost increasing samples:', m2)
+            print('Initial temperature T0:' , self.T)
+            print('-'*50)
+
+            if self.verbose > 1:
+                print('(!) Visulizing the initial state.')
+                print('(>) Close the figure to continue ...')
+                self.chip.draw_chip()
+
+    def update_temperature(self):
+        ''' Temperature update based on Huang et al. [1986]
+        '''
+        if self.rejection > 0.01 * self.max_iter:
+            self.T=self.T*100
+        else:
+            if round(self.T, 10)*(1.0*self.current_delta_cost/self.sigma**2)<100:
+
+                self.T = self.T * exp(round(self.T, 10)* \
+                (1.0*self.current_delta_cost/self.sigma**2))
+            else:
+                self.T = self.T * exp(100)
+
+    def random_swap(self):
+
+        [i,j] = RND.sample(range(self.chip.num_cells),2)
+
+        delta_cost = self.chip.swap_delta_cost(i,j)
+
+        return i,j, delta_cost
+
+    def make_move(self):
+
+        cell_i, cell_j, delta_cost = self.random_swap()
+
+        if delta_cost < 0:
+            self.last_good_move = self.num_iter
+
+        if self.T < 0.000000000000001:
+            if delta_cost > 0:
+                check = 0
+            else:
+                check = 1
+        else:
+            check = exp(-delta_cost*(self.num_iter - self.last_good_move)/round(self.T,14))
+        
+
+        if RND.uniform(0, 1) <= check:
+            self.chip.commit_swap_cells(cell_i,cell_j)
+            self.current_delta_cost = delta_cost
+            self.accepted = True
+            self.rejection=0
+        else:
+            self.accepted = False
+            self.rejection +=1
+
+        self.update_temperature()
+
+    def anneal(self):
+        self.param_initialization()
+
+        if self.verbose > 0:
+            if self.num_iter % int(floor(0.01 * self.max_iter)) == 0:
+                print('>>> Temp:', self.T,', Iter:', self.num_iter,\
+                ', Cost:' , self.chip.total_cost)
+                
+        while (self.num_iter <= self.max_iter) and \
+         (self.no_change <= 0.05 * self.max_iter):
+
+
+            self.make_move()
+
+            # Book keeping section for SA:
+            self.num_iter += 1
+
+            if self.accepted == False:
+                self.num_rejection += 1
+            else:
+                self.num_acceptance += 1
+
+            if (absolute(self.current_delta_cost) < self.epsilon):
+                self.no_change += 1
+            else:
+                self.no_change = 0
+
+
+            # Printing every 1% of the whole process ...
+            if self.verbose > 0:
+                if self.num_iter % int(floor(0.01 * self.max_iter)) == 0:
+                    print('>>> Temp:', self.T,', Iter:', self.num_iter,\
+                    ', Cost:' , self.chip.total_cost)
+
+            if self.verbose > 1:
+                if self.num_iter % int(floor(0.1 * self.max_iter)) == 0:
+                    print('(!) Visulizing the current state.')
+                    print('(>) Close the figure to continue ...')
+                    self.chip.draw_chip()
+
+        # After SA:
+        if self.verbose > 1:
+            print('(!) Visulizing the current state.')
+            print('(>) Close the figure to continue ...')
+            self.chip.draw_chip()
+
+        return self.chip.total_cost
---
+++ chip_class.py
@@ -0,0 +1,190 @@
+''' This module defines a chip object that contains information about its cells,
+The size of the chip, the net lists and position of each cell on the chip.'''
+import random as random
+from numpy import floor
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class chip():
+    def __init__(self, rows, cols, cells, conns, net_list):
+
+        # initialize object constants:
+        self.num_rows = rows
+        self.num_cols = cols
+        self.num_cells = cells
+        self.num_nets = conns
+        self.net_list = net_list
+
+        # initialize object variables:
+        self.area = self.num_rows * self.num_cols
+
+        self.cell_location={}
+        self.reset_cell_location()
+
+        self.cell_net_incidence()
+
+        # Calculate the current total cost:
+        self.cost_half_perim()
+
+        # Prepare for drawing the chip:
+        self.pre_draw()
+
+    def reset_cell_location(self):
+        ''' This function resets the location of the cells to a randomly chosen
+        location.'''
+
+        # Empty the current location dict:
+        self.cell_location={}
+
+        # create random locations:
+        indices = random.sample(xrange(self.area), self.num_cells)
+        random_locations =  list(range(self.area)[i] for i in indices)
+
+        # resent all locations to their random locations:
+        for i in range(self.num_cells):
+            y = int(floor(random_locations[i]*1.0 / self.num_cols))
+            x = int(random_locations[i] - y * self.num_cols)
+            self.cell_location[i] = (x,y)
+
+    def calculate_bounding_box(self, net_ID):
+        ''' This function calculates the dimensions of the bounding box for a
+        particular net.'''
+
+        rows = []
+        cols = []
+
+        # Loop over all cells in the net
+        for cell in self.net_list[net_ID]:
+            cols.append(self.cell_location[cell][0])
+            rows.append(self.cell_location[cell][1])
+
+        # find the left most, right most, highest and lowest cells in the net:
+        # Then calculate the bounding box dimension:
+        delta_y = max(rows) - min(rows)
+        delta_x = max(cols) - min(cols)
+
+        return delta_x + delta_y
+
+    def cost_half_perim(self):
+
+        ''' this function calculates the half perimeter cost for all nets'''
+
+        self.total_cost=0
+
+        for net in range(self.num_nets):
+            self.total_cost += self.calculate_bounding_box(net)
+
+    def subcost_half_perim(self,i,j):
+        ''' This function is designed to calculate the half perimeter cost only
+        for the nets affected by the swap of cell i and j'''
+
+        nets_affected = []
+
+        # Find the nets affected by this change:
+        nets_affected = self.incidence[i] + self.incidence[j]
+
+        nets_affected= list(set(nets_affected))
+
+        current_net_aff_cost = 0
+
+
+        for net in nets_affected:
+            current_net_aff_cost += self.calculate_bounding_box(net)
+
+        return current_net_aff_cost
+
+    def swap_cells(self,i,j):
+        '''This function swaps two cells'''
+
+        temp_1 = self.cell_location[i]
+        temp_2 = self.cell_location[j]
+
+        self.cell_location[i]= temp_2
+        self.cell_location[j]= temp_1
+
+    def commit_swap_cells(self, i, j):
+        ''' This function finalizes a cell swap by incorporating it into the
+        cost function'''
+
+        #update Cost
+        self.total_cost += self.swap_delta_cost(i,j)
+
+        # Do the swap:
+        self.swap_cells(i,j)
+
+
+    def cell_net_incidence(self):
+        '''This function creates a dictionary of cells and the corresponding
+        nets in which each cell appears. This is used to understand which nets
+        are affected when a swap of a paricular cell is happening:'''
+
+        self.incidence = {}
+
+        for net in range(self.num_nets):
+            for cell in self.net_list[net]:
+                if cell in self.incidence.keys():
+                    self.incidence[cell].append(net)
+                else:
+                    self.incidence[cell] = [net]
+
+    def swap_delta_cost(self,i,j):
+        ''' This function calculates the diference in cost after swapping 2
+        cells. In its current form it calculates the whole cost function for all
+        nets at each point. This can be improved a lot by only chaning the cost
+        for the affected cells.'''
+
+        # store the sub cost according to i and j before the swap
+        current_cost = self.subcost_half_perim(i,j)
+
+        # do the swap:
+        self.swap_cells(i,j)
+
+        # store the cost after the swap:
+        future_cost = self.subcost_half_perim(i,j)
+
+        # return everything back to its previous location:
+        self.swap_cells(i,j)
+
+        return future_cost - current_cost
+
+    def pre_draw(self):
+        ''' preprocess an edge-less networkx grid graph object based on the
+        specifications of the chip.'''
+
+        self.G = nx.grid_2d_graph(self.num_cols, self.num_rows)
+
+        self.pos = dict( (n, n) for n in self.G.nodes() )
+
+        self.G.remove_edges_from(self.G.edges())
+
+    def draw_chip(self):
+        ''' This fucntion draws the grid of the chip and highlights the location
+        of different cells'''
+
+        labels = {}
+
+        for key in range(self.num_cells):
+            labels[self.cell_location[key]]=key
+
+        colors=[]
+        for node in self.G.nodes():
+
+            if node in self.cell_location.values():
+                colors.append('#f2325f')
+            else:
+                colors.append('#A0CBE2')
+
+        nx.draw_networkx(self.G, pos=self.pos, labels=labels,font_size=6,\
+        node_size=150,node_color = colors)
+
+        plt.axis('off')
+        plt.show()
+
+    def display(self):
+        ''' For debigging perpusos displays the current state of the chip.'''
+        
+        print('Current Cell locations:')
+        print(self.cell_location)
+
+        print('The current cost:')
+        print(self.total_cost)
