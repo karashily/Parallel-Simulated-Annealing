fe2605313c7d443924e5c3ac74de627f9f565d00
double <mohamed.ahmed.geek@gmail.com>
2020-05-31 12:32:33 +0200

    modify cost

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from multiprocessing import Process,Value,Lock,Manager,Lock
 
 import chip as chip_class
 import placer as placer_class
+import copy
 
 
 if __name__ == "__main__":
@@ -21,41 +22,48 @@ if __name__ == "__main__":
         # 'Examples/paira.txt',
         # 'Examples/pairb.txt',
         # 'Examples/apex4.txt']
-        Benchmarks = ['Examples/paira.txt']
-        threadsNo = 8
+        Benchmarks = ['Examples/alu2.txt']
+        # threadsNo = 
         # Main loop over all benchmarks:
         for inputFile in Benchmarks:
             chip = chip_class.Chip.loadChip(inputFile)
             costs = []
             times = []
-            for _ in range(1):
-                SAs = []
-                SA = placer_class.Placer(chip, 5*10**5 / threadsNo)
 
-                T, sigma = placer_class.Placer.initParam(20000, chip)
-                stateLock = Lock()
-                results = manager.dict()
-                
-                processes = []
-                for i in range(threadsNo):
-                    p = Process(target=SA.anneal, args=(stateLock, chip, T, sigma, i, results))
-                    processes.append(p)
-                
-                t0 = time.time()
-                for p in processes:    
-                    p.start()
-                for p in processes:
-                    p.join()
-                t1 = time.time()
-                t = t1 - t0
+            threadsNos = [1,2,4,8]
+            T, sigma = placer_class.Placer.initParam(20000, chip)
+            c = copy.deepcopy(chip)
+            for threadsNo in threadsNos:
+                for _ in range(5):
+                    SAs = []
+                    SA = placer_class.Placer(chip, 5*10**5 / threadsNo)
 
-                cost = results[0]
-                for i in range(threadsNo):
-                    if(results[i] < cost):
-                        cost = results[i]
-                costs.append(cost)
-                times.append(t)
+                    stateLock = Lock()
+                    results = manager.dict()
+                    
+                    processes = []
+                    for i in range(threadsNo):
+                        p = Process(target=SA.anneal, args=(stateLock, chip, T, sigma, i, results))
+                        processes.append(p)
+                    
+                    t0 = time.time()
+                    for p in processes:    
+                        p.start()
+                    for p in processes:
+                        p.join()
+                    t1 = time.time()
+                    t = t1 - t0
 
-            print('Placement Done for chip:', inputFile, "with threadsNo", threadsNo)
-            print('avg cost =', mean(costs))
-            print('avg time elapsed =' , mean(times))
+                    cost = results[0]
+                    for i in range(threadsNo):
+                        if(results[i] < cost):
+                            cost = results[i]
+                    costs.append(cost)
+                    times.append(t)
+                    chip = c
+
+                print('Placement Done for chip:', inputFile, "with threadsNo", threadsNo)
+                print('avg cost =', mean(costs))
+                print('avg time elapsed =' , mean(times))
+                costs = []
+                times = []
